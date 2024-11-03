5a18c1d4191af32d3b2e6766a83d6495f24974cd
coomzilla <THIHyb@gmail.com>
2024-11-04 00:18:51 +0200

    Wrong SQL code

--- migrations/20240311256600-init-db-table.sql
+++ migrations/20240311256600-init-db-table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS public.cats
 (
-    id integer NOT NULL DEFAULT nextval('cats_id_seq'::regclass),
-    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    description text COLLATE pg_catalog."default" NOT NULL,
+    id serial NOT NULL,
+    name character varying(50) NOT NULL,
+    description text,
     age integer NOT NULL,
     CONSTRAINT cats_pkey PRIMARY KEY (id)
-)
+);
